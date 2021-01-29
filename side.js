8a2484fce8a731b39fb779ea822ed4eb89bed21d
yossefzedan <68157653+yossefzedan@users.noreply.github.com>
2021-01-29 10:23:12 +0200

    Add files via upload

---
+++ side.js
@@ -0,0 +1,8 @@
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+$("#menu-toggle2").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
